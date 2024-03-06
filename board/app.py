bae1e58b0c07d8eda623201b6975c519883cfb77
harshavardhanreddybindela <harshavardhanreddy.bindela@slu.edu>
2024-03-06 00:43:34 -0600

    Email and Export CSV functionalities

--- board/app.py
+++ board/app.py
@@ -1,21 +1,68 @@
-# Import necessary modules and libraries
+from curses import flash
+import smtplib
+from flask_session.__init__ import Session
+from werkzeug.security import check_password_hash
+from flask_mail import Mail, Message 
 import os
-# os.chmod('board/media/profile', 755)
 import sys
-from flask import Flask, render_template, request, redirect, url_for, jsonify,flash
-from werkzeug.utils import secure_filename
+import csv
+import io
+from flask import Flask, render_template, request, redirect, session, url_for, jsonify,Response
 import pymysql
-from config.db import DATABASE_CONFIG  # Import database configuration
-# Initialize Flask app
+from config.db import DATABASE_CONFIG
+from db_functions import get_branch_list, get_email, get_qualification_list, get_students,get_delete_hobbies,db_connection, post_edit_hobbies,post_student,post_hobbies,get_delete_student,get_hobbies_list,edit_student,get_student_hobbies,get_student_details
+
+# # Initialize Flask app
 app = Flask(__name__)
+# mail = Mail(app)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+conn = db_connection()
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'harshareddy4400@gmail.com'
+app.config['MAIL_PASSWORD'] = 'gdzc jhpd tyqs aaks'
+mail = Mail(app)
 
 
-# Create a MySQL database connection using the configuration from DATABASE_CONFIG
-conn = pymysql.connect(**DATABASE_CONFIG)
+@app.route("/sendmail/<int:id>")
+def sendmail(id):
+    conn = db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT student_id,email FROM student WHERE student_id = %s", (id,))
+    result = cursor.fetchone()
+    # print(result)
+    if result:
+        user_email = result['email']
+        # print(user_email)
+        msg = Message('Test',
+                  sender='harshareddy4400@gmail.com',
+                  recipients=[user_email],
+                  )
+        
+        msg.body = 'Hello Flask message sent from Flask-Mail'
+        mail.send(msg)
+    else:
+        return 'No user found to send email to.'
+    return 'Sent'
+
+
+
+
+@app.route('/home')
+def home():
+    if 'fname' in session:
+        welcomeMsg = f"Welcome, {session['fname']}!"
+    else:
+        welcomeMsg = "Welcome!"
+    return render_template('home.html', message=welcomeMsg)
+
 
 @app.route('/add_student')
 def add_student():
-    conn = pymysql.connect(**DATABASE_CONFIG)
     cursor = conn.cursor()
     try:
         data={}
@@ -30,175 +77,144 @@ def add_student():
         print("Error fetching branches,qualification,hobbies:", e)
         return render_template('add_student.html', data=data)
     finally:
-        cursor.close()
         conn.close()
 
 
 # Define a route for the registration form
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    # Extract student info from form data
+    student_info = request.form.to_dict()
     if request.method == 'POST':
-        # Retrieve data from the HTML form
-        # profile =request.form['profile']
-        # raise SystemExit
         
         file = request.files['profile']
         if file.filename != '':
-            # BASE_DIR ='/media/'
-            # MEDIA_ROOT = BASE_DIR / 'profile/'
             file.save(file.filename)
-            profile ='abc.jpg'
-        fname = request.form['fname'] #request is a class in built-in libraries
-        lname = request.form['lname']
-        email = request.form['email']
-        phone = request.form.get('phone', '')
-        branch = request.form['branch']
-        qualification = request.form['qualification']
-        gender = request.form.get('gender', '')
-        dob = request.form['dob']
-        address = request.form['address']
-        city = request.form['city']
-        pincode = request.form['pincode']
-        country = request.form['country']
-        hobbies = '|'.join(request.form.getlist('hobbies[]'))
-        
-        # if 'profile' in request.files:
-        #     file = request.files['profile']
-        #     if file.filename == '':
-        #         flash('No selected file')
-        #         return redirect(request.url)
-        #     if file:  # If a file is present
-        #         filename = secure_filename(file.filename)
-        #         save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        #         file.save(save_path)
-        #         # Now you have the path where the file is saved
-        #         profile_path = save_path("/board/media/profile")
-        # if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        #     os.makedirs(app.config['UPLOAD_FOLDER'])
+            student_info['profile'] ='abc.jpg'
+        student_info['fname'] = request.form['fname'] #request is a class in built-in libraries
+        student_info['lname'] = request.form['lname']
+        student_info['email'] = request.form['email']
+        student_info['phone'] = request.form.get('phone', '')
+        student_info['branch'] = request.form['branch']
+        student_info['qualification'] = request.form['qualification']
+        student_info['gender'] = request.form.get('gender', '')
+        student_info['dob'] = request.form['dob']
+        student_info['address'] = request.form['address']
+        student_info['city'] = request.form['city']
+        student_info['pincode'] = request.form['pincode']
+        student_info['country'] = request.form['country']
+        student_info['hobbies'] = '|'.join(request.form.getlist('hobbies[]'))
 
         try:
             with conn.cursor() as cursor:
-                # Create a new record
-                sql = """INSERT INTO student (profile,fname, lname, email, phone, gender, dob, address, city, pincode, country, hobbies, branch_name, qualification)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s,%s)"""
-                sql1=cursor.execute(sql, (profile,fname, lname, email, phone, gender, dob, address, city, pincode, country, hobbies, branch,qualification))
-                print(sql1)
-                conn.commit()  # Commit changes
-                return "Registration successful"
+                
+                data = {}
+                insert_student_query= post_student()
+                data['add_student']=cursor.execute(insert_student_query,(student_info['profile'],student_info['fname'], student_info['lname'], student_info['email'], student_info['phone'], student_info['gender'], student_info['dob'], student_info['address'], student_info['city'], student_info['pincode'], student_info['country'], student_info['branch'],student_info['qualification']))
+                student_id = cursor.lastrowid
+                print(data['add_student'])
+                for hobby_id in student_info['hobbies'].split('|'):
+                    insert_hobby_query = post_hobbies()
+                    data['post_hobbies']=cursor.execute(insert_hobby_query, (student_id, hobby_id))
+                conn.commit()
+                email=student_info['email']
+                fname=student_info['fname']
+                print(fname)
+                msg = Message('Thank You for Your Submission',
+                      sender='harshareddy4400@gmail.com',
+                      recipients=[email])
+                msg.body = """
+                Hello {}!,
+                    Thank you for your submission. You are registered as a student in our website \t
+                Thanks and Regards,
+                Harsha
+                Note:this is a test run by Harsha Reddy""".format(fname)
+                mail.send(msg)
         except Exception as e:
+            conn.rollback()  # Rollback the transaction in case of an error
             return f"Error: {str(e)}"
-    return render_template('add_student.html')
-    
+    return render_template('add_student.html',data=data)
 @app.route('/list/', defaults={'page': 1}, methods=['GET', 'POST'])
 @app.route('/list/page/<int:page>', methods=['GET'])
 def list_students(page):
-    conn = pymysql.connect(**DATABASE_CONFIG)
+    conn = db_connection()
     cursor = conn.cursor()
     id = request.args.get('id', None)
     sort = request.args.get('sort', 'student_id')
     order = request.args.get('order', 'asc').lower()
     current_sort = request.args.get('current_sort')
     current_order = request.args.get('current_order', 'asc')
+    prev_page = max(page - 1, 1)
+    next_page = page + 1
+    offset = 4 * (page - 1)
+
     if sort == current_sort:
         order = 'desc' if current_order == 'asc' else 'asc'
     valid_sort_columns = ['student_id', 'fname', 'lname', 'email', 'phone', 'branch_name', 'qualification', 'dob', 'address', 'city', 'pincode', 'country']
     if sort not in valid_sort_columns or order not in ['asc', 'desc']:
         sort = 'student_id','fname'
         order = 'asc'
-
+        
     try:
         data = {}
-        if id is None:
-            prev_page = page - 1
-            next_page = page + 1
-            query = f"SELECT * FROM student ORDER BY {sort} {order} LIMIT 4 OFFSET %s"
-            cursor.execute(query, (4 * (page - 1),))
-            data['studentlist'] = list(cursor.fetchall())
-            return render_template('student_list.html', page=page,data=data, prev_page=prev_page, next_page=next_page, id=id, sort=sort, order=order)
-        else:
-            query = f"SELECT * FROM student WHERE student_id = %s ORDER BY {sort}"
-            cursor.execute(query, (id,))
-            data['studentlist'] = cursor.fetchall()
-            return render_template('student_list.html', data=data, id=id, sort=sort, order=order)  # Including sort/order in rendering
+        print("*******")
+        student_list_query = get_students(id=id, sort=sort, order=order, offset=offset)
+        cursor.execute(student_list_query, (offset,))
+        data['studentlist'] = cursor.fetchall()
+        data['all_hobbies'] = get_hobbies_list(cursor)
+        data['hobbies_dict'] = dict((hobby['hobbies_id'], hobby['hobby']) for hobby in data['all_hobbies'])
+        # print(data['studentlist'])
+        return render_template('student_list.html', page=page, data=data, prev_page=prev_page, next_page=next_page)
     except Exception as e:
         print(f"Error: {e}")
         return render_template('error_page.html', error_message=str(e))
     finally:
         conn.close()
 
-
 # Define a route for deleting a student
 @app.route('/delete/<int:id>', methods=['GET'])
 def delete_student(id):
-    try:#Fatal,warning,notice Error #Check this for python
-        with conn.cursor() as cursor:
-            # Delete record
-            sql = "DELETE FROM student WHERE student_id = %s"
-            print(sql)
-            cursor.execute(sql, (id,))
-            conn.commit()
-        # Redirect to the list page with a success message
+    try:
+        cursor = conn.cursor()
+        delete_query = get_delete_student(id)
+        cursor.execute(delete_query, (id,))
+        conn.commit()
         return redirect(url_for('list_students', msg="Student deleted successfully"))
-    except Exception:
-        # Redirect to the list page with an error message if something goes wrong
-        return redirect(url_for('list_students', msg=f"Error: In Deleting Student"))
+    except Exception as e:
+        print(f"Error: {e}")
+        return redirect(url_for('list_students', msg="Error: In Deleting Student"))
+    finally:
+        if conn:
+            conn.close()
 
 
-def fetch_hobbies(cursor):
-    cursor.execute("SELECT * FROM hobbies ORDER BY hobby ASC")
-    return cursor.fetchall()
-
-def fetch_student_hobbies(cursor, student_id):
-    cursor.execute("SELECT hobbies FROM student WHERE student_id = %s", (student_id,))
-    result = cursor.fetchone()
-    if result and result['hobbies']:
-        return result['hobbies'].split(',')
-    return []
 # Define a route for editing a student
 @app.route('/edit_student/<int:id>', methods=['GET'])
 def edit_student(id):
-    conn = pymysql.connect(**DATABASE_CONFIG)
+    conn = db_connection()
     try:
+        data={}
         with conn.cursor(pymysql.cursors.DictCursor) as cursor:
-            # Fetch student details
-            all_hobbies = fetch_hobbies(cursor)
-            selected_hobbies = fetch_student_hobbies(cursor, id)
-            # print(selected_hobbies)
-            # print(all_hobbies)
-            cursor.execute("SELECT * FROM student WHERE student_id = %s", (id,))
-            student = cursor.fetchone()
+            data['all_hobbies'] = get_hobbies_list(cursor)
+            data['selected_hobbies'] = get_student_hobbies(cursor, id)
+            data['branch_list'] = get_branch_list(cursor)
+            data['qualification_list'] = get_qualification_list(cursor)
+            student = get_student_details(cursor, id)
             if not student:
                 return "Student not found", 404
-            
-            # Correctly prepare the selected hobbies list
-            selected_hobbies = student['hobbies'].split('|') if student['hobbies'] else []
-
-            # Fetch branches
-            cursor.execute("SELECT * FROM branches ORDER BY branch_id ASC")
-            branchlist = cursor.fetchall()
-
-            # Fetch qualifications
-            cursor.execute("SELECT * FROM qualifications ORDER BY qualification_id ASC")
-            qualificationlist = cursor.fetchall()
-
-            # Fetch hobbies
-            # cursor.execute("SELECT * FROM student ORDER BY hobbies ASC")
-            # hobbies = cursor.fetchall()
-
-            return render_template('edit_student.html', student=student, selected_hobbies=selected_hobbies,
-                                   branchlist=branchlist, qualificationlist=qualificationlist, all_hobbies=all_hobbies)
+            data['student'] = student
+            return render_template('edit_student.html', student=student, data=data)
     except Exception as e:
-        print(f"Error: {e}")
         return "Error fetching data", 500
     finally:
         conn.close()
-
 
 # Define a route for updating a student's information
 @app.route('/update_student/<int:id>', methods=['POST'])
 def update_student(id):
     if request.method == 'POST':
-        # Extract form data
+        # Retrieve form data
+        profile = request.form['profile']
         fname = request.form['fname']
         lname = request.form['lname']
         email = request.form['email']
@@ -211,34 +227,43 @@ def update_student(id):
         city = request.form['city']
         pincode = request.form['pincode']
         country = request.form['country']
-        hobbies = '|'.join(request.form.getlist('hobbies[]'))
+        hobbies = request.form.getlist('hobbies[]')
 
         conn = None
         try:
-            conn = pymysql.connect(**DATABASE_CONFIG)
+            data={}
+            conn = db_connection()
             with conn.cursor() as cursor:
-                # Update the student record
-                sql = """
-                UPDATE student 
-                SET fname=%s, lname=%s, email=%s, phone=%s, gender=%s, dob=%s, 
-                    address=%s, city=%s, pincode=%s, country=%s, hobbies=%s, 
-                    qualification=%s, branch_name=%s 
-                WHERE student_id=%s
-                """
-                cursor.execute(sql, (fname, lname, email, phone, gender, dob, address, city, pincode, 
-                                     country, hobbies, qualification, branch_name, id))
-                conn.commit()
-                print('Student updated successfully.', 'success')
+                update_query = """UPDATE student
+                                SET profile=%s, fname=%s, lname=%s, email=%s, phone=%s,
+                                gender=%s, dob=%s, address=%s, city=%s, pincode=%s,
+                                country=%s, qualification=%s, branch_name=%s
+                                WHERE student_id=%s"""
+                data['update']=cursor.execute(update_query, (profile, fname, lname, email, phone, gender, dob,
+                                              address, city, pincode, country, qualification,
+                                              branch_name, id))
+
+                # Delete existing hobbies for the student
+                delete_hobbies_query = "DELETE FROM student_hobbies WHERE student_id = %s"
+                cursor.execute(delete_hobbies_query, (id,))
+
+                # Insert new hobbies for the student
+                insert_hobby_query = "INSERT INTO student_hobbies (student_id, hobbies_id) VALUES (%s, %s)"
+                for hobby_id in hobbies:
+                    cursor.execute(insert_hobby_query, (id, hobby_id))
+            conn.commit()
+            print('Student updated successfully.')
         except Exception as e:
-                print(f"Error updating student: {str(e)}", 'error')
+            print(f"Error updating student: {str(e)}")
+            conn.rollback()  # Rollback the transaction in case of an error
         finally:
             if conn:
                 conn.close()
         return redirect(url_for('list_students'))
-        
+
 @app.route('/email_check', methods=['GET'])
 def email_check():
-    conn = pymysql.connect(**DATABASE_CONFIG)
+    conn = db_connection()
     cursor = conn.cursor()
     try:
         email = request.args.get('emailVal')
@@ -264,6 +289,73 @@ def email_check():
             cursor.close()
             conn.close()
     return True
+
+@app.route('/export_students_csv')
+def export_csv():
+    conn = db_connection()
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("""SELECT s.*,group_concat(hobbies_id separator '|') as hobbies FROM student s
+                JOIN student_hobbies sh ON s.student_id = sh.student_id
+                GROUP BY s.student_id""")
+    result = cursor.fetchall()
+    si = io.StringIO()
+    cw = csv.writer(si)
+    print(result)
+    cw.writerow(result[0].keys())
+    for row in result:
+        cw.writerow(row.values())
+
+    output = si.getvalue()
+    conn.close()
+
+    return Response(
+        output,
+        mimetype="text/csv",
+        headers={"Content-Disposition": "attachment;filename=student_list.csv"}
+    )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    conn = None
+    try:
+        if request.method == 'POST':
+            email = request.form['email']
+            password = request.form['password']
+            conn = db_connection()
+            with conn.cursor() as cursor:
+                cursor.execute('SELECT student_id, fname, email, password FROM student WHERE email = %s', (email,))
+                user = cursor.fetchone()
+            
+            if user and user['password'] == password:
+                session['user_id'] = user['student_id']
+                session['username'] = user['email']
+                session['fname'] = user['fname']
+                return redirect(url_for('home'))
+            else:
+                error = "Invalid email or password"
+    except Exception as e:
+        error = "An error occurred: {}".format(str(e))
+    finally:
+        if conn:
+            conn.close()
+    return render_template('login.html', error=error)
+    
+    
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
