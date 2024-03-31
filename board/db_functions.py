f1e280510ab018137ed5fd74e40fe1ea8675d78c
Harsha Vardhan Reddy Bindela <harshavardhanreddy.bindela@slu.edu>
2024-03-31 15:13:09 -0500

    Final Commit

--- board/db_functions.py
+++ board/db_functions.py
@@ -6,7 +6,6 @@ from config.db import DATABASE_CONFIG
 
 def db_connection():
     conn = pymysql.connect(**DATABASE_CONFIG)
-    
     return conn
 
 # post operations
@@ -22,38 +21,38 @@ def post_edit_hobbies():
     insert_hobbies_query = "INSERT INTO student_hobbies (student_id, hobbies_id) VALUES (%s, %s)"
     return insert_hobbies_query
 
-# student get operations
-def get_students(id=None, sort='student_id', order='asc', offset=0, limit=4):
-    if id is None:
-        student_list = """
-            SELECT s.student_id, fname, lname, email, phone, gender, DATE_FORMAT(dob, '%%d-%%m-%%Y') AS dob,
-            address, city, pincode, country, branch_name, qualification,
-            created_by, DATE(created_on) AS created_date,
-            GROUP_CONCAT(h.hobby SEPARATOR ', ') AS hobbies_string
-            FROM student s
-            LEFT JOIN student_hobbies sh ON sh.student_id = s.student_id
-            LEFT JOIN hobbies h ON h.hobbies_id = sh.hobbies_id
-            GROUP BY s.student_id
-            ORDER BY {} {} LIMIT %s OFFSET %s""".format(sort,order)
+def get_students(sort='student_id', order='asc', offset=0, limit=4, search=None):
+    base_query = """
+    SELECT s.student_id, s.fname, s.lname, s.email, s.phone, s.gender, DATE_FORMAT(s.dob, '%%d-%%m-%%Y') AS dob, s.address,
+    s.city, s.pincode, s.country, s.branch_name, s.qualification,s.created_by, DATE(s.created_on) AS created_date,
+    GROUP_CONCAT(h.hobby SEPARATOR ', ') AS hobbies_string FROM student s
+    LEFT JOIN student_hobbies sh ON sh.student_id = s.student_id
+    LEFT JOIN hobbies h ON h.hobbies_id = sh.hobbies_id
+    """
+    
+    search_query = ""
+    if search:
+        search_query = """
+            WHERE fname LIKE %s OR lname LIKE %s OR email LIKE %s
+        """
+    
+    group_order_limit_query = """
+        GROUP BY s.student_id, s.fname, s.lname, s.email, s.phone, s.gender, s.dob, s.address, s.city, s.pincode, s.country,
+        s.branch_name, s.qualification, s.created_by, s.created_on
+        ORDER BY {} {} LIMIT %s OFFSET %s
+    """.format(sort, order)
+    
+    final_query = base_query + search_query + group_order_limit_query
+    return final_query
 
-    else:
-        student_list ="""
-            SELECT s.student_id, fname, lname, email, phone, gender, DATE_FORMAT(dob, '%%d-%%m-%%Y') AS dob, address, city, pincode, country, branch_name, qualification,
-            created_by, DATE(created_on) AS created_date, GROUP_CONCAT(h.hobby SEPARATOR ', ') AS hobbies_string
-            FROM student s
-            JOIN student_hobbies sh ON s.student_id = sh.student_id
-            WHERE s.student_id = %s
-            GROUP BY s.student_id
-            ORDER BY {} {} LIMIT %s OFFSET %s
-        """.format(sort, order)
-    return student_list
 
 
 
 def get_student_details(cursor, student_id):
     query = "SELECT * FROM student WHERE student_id = %s"
     cursor.execute(query, (student_id,))
-    return cursor.fetchone()  # Assuming this returns a dictionary
+    result = cursor.fetchone()
+    return result if result else {}
 
 
 def get_student_email(cursor, email):
@@ -63,12 +62,12 @@ def get_student_email(cursor, email):
 
 
 def get_student_hobbies(cursor, student_id):
-    get_student_hobbies_query = """SELECT GROUP_CONCAT(hobbies_id SEPARATOR '|') AS hobby_ids
+    get_student_hobbies_query = """SELECT GROUP_CONCAT(hobbies_id SEPARATOR '|') AS hobby_id
             FROM student_hobbies
             WHERE student_id = %s"""
     cursor.execute(get_student_hobbies_query, (student_id,))
     result = cursor.fetchone()
-    return result['hobby_ids'].split('|')
+    return result['hobby_id']
 
 def get_details():
     list_query="""SELECT s.*, group_concat(h.hobby separator '|') as hobbies
@@ -95,10 +94,6 @@ def get_hobbies_list(cursor):
     get_hobbies_query = "SELECT hobbies_id, hobby FROM hobbies ORDER BY hobby ASC"
     cursor.execute(get_hobbies_query)
     return cursor.fetchall()
-
-# def get_delete_hobbies(id):
-#     delete_hobbies_query = "DELETE FROM student_hobbies WHERE student_id = %s"
-#     return delete_hobbies_query
 
 def get_branch_list(cursor):
     branch_list_query = "SELECT * FROM branches ORDER BY branch_id ASC"
